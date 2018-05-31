0c53a1e1301593decbaca02d0b37dcf41e4d80c4
domsellen <domsellen@gmail.com>
2018-05-31 17:32:43 +0200

    signIn/signOut was not finished. Removed it for now.

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ResumeLoad from './containers/CVContainerLoad';
 import ResumeNew from './components/resumeNew';
 import NotFound from './components/notFound';
-import SignIn from './components/signIn';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(promise, reduxThunk)(createStore);
@@ -22,7 +21,6 @@ ReactDOM.render(
           <Route path="/resume/:id" component={ResumeLoad} />
           <Route path="/notFound" component={NotFound} />
           <Route path="/new" component={ResumeNew} />
-          <Route path="/signin" component={SignIn} />
           <Route path="/" component={ResumeLoad} />
         </Switch>
       </div>
