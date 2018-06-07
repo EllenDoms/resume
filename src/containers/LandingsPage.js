7d95a961646ce0b53a01338af68754c6c217169f
domsellen <domsellen@gmail.com>
2018-06-07 11:05:20 +0200

    Landingspage

--- src/containers/LandingsPage.js
+++ src/containers/LandingsPage.js
@@ -12,7 +12,7 @@ import step3 from '../img/icon/send.png';
 import design1 from '../img/cv/demo1.png';
 
 
-export default class LandingPage extends Component {
+export default class LandingsPage extends Component {
   render(){
     return(
       <div id="LandingsPage" className="builderCss">
@@ -25,8 +25,8 @@ export default class LandingPage extends Component {
               </ul>
             </div>
             <div id="tagline">
-              <h1>Build Your resume. <br/>Get the job.</h1>
-              <p>(Yes, it's that simple)</p>
+              <h1 className="fontsforweb_bignoodletitling">Build Your resume. <br/>Get the job.</h1>
+              <p className="fontsforweb_bignoodletitling">(Yes, it's that simple)</p>
               <Link to={`/login`} className="btn btn-primary">Create account</Link>
               <Link to={`/resume/resume`} className="btn btn-secondary">See demo</Link>
             </div>
@@ -35,7 +35,7 @@ export default class LandingPage extends Component {
         </div>
         <div id="stepsBlock" className="block white center">
           <div className="container">
-            <h2 className="center">15 minutes. 3 easy steps.</h2>
+            <h2 className="fontsforweb_bignoodletitling center">15 minutes. 3 easy steps.</h2>
             <ul>
               <li><img src={step1} /><p>Add your info.</p></li>
               <li><img src={step2} /><p>Choose a design.</p></li>
@@ -45,12 +45,12 @@ export default class LandingPage extends Component {
         </div>
         <div id="designsBlock" className="block grey">
           <div className="container">
-            <img src={design1} className="designImg" />
             <div className="right">
-              <h3>Ellen Doms</h3>
-              <h4>Salmon design</h4>
-              <div className="quote">“I've build this tool because I needed it myself. I didn't find another resume builder with designs that stood out.”</div>
+              <h3 className="fontsforweb_bignoodletitling">Ellen Doms</h3>
+              <h4 className="fontsforweb_bignoodletitling">Salmon design</h4>
+              <div className="quote">“I've built this tool because I needed it myself. I didn't find another resume builder with designs that stood out.”</div>
             </div>
+            <img src={design1} className="designImg" />
           </div>
         </div>
         <div id="footer" className="block dark">
